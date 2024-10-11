db754ead687162d38b89ebee69b892755864a115
xuh5 <84805264+xuh5@users.noreply.github.com>
2024-10-11 03:03:11 -0500

    add_database
    1

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,17 @@ import 'bootstrap-icons/font/bootstrap-icons.css';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { useJsonQuery } from './utilities/fetch';
+import { useDbData } from "./utilities/firebase";
 import TermPage from './components/TermPage';
 import CourseForm from './components/CourseForm'; 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 const queryClient = new QueryClient();
 const App = () => {
-  const [data, isLoading, error] = useJsonQuery('https://courses.cs.northwestern.edu/394/guides/data/cs-courses.php');
-  if (error) return <h1>Error loading course data: {`${error}`}</h1>;
-  if (isLoading) return <h1>Loading course data...</h1>;
-  if (!data) return <h1>No course data found</h1>;
+  const [data, error] = useDbData('/');
+
+  if (error) return <h1>Error loading data: {error.toString()}</h1>;
+  if (data === undefined) return <h1>Loading data...</h1>;
+  if (!data) return <h1>No data found</h1>;
   return(  
     <QueryClientProvider client={queryClient}>
       <Router> 
---
+++ src/utilities/firebase.js
@@ -0,0 +1,50 @@
+import { getDatabase, onValue, ref, update, get, push, set ,remove} from 'firebase/database';
+import { useCallback, useEffect, useState } from 'react';
+import { initializeApp } from 'firebase/app';
+
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAUfRDxoX9nd3GRH2ZgrgenZIPEAavMMZw",
+    authDomain: "cs392-react-haichen.firebaseapp.com",
+    databaseURL: "https://cs392-react-haichen-default-rtdb.firebaseio.com",
+    projectId: "cs392-react-haichen",
+    storageBucket: "cs392-react-haichen.appspot.com",
+    messagingSenderId: "313610334645",
+    appId: "1:313610334645:web:1241016a1436e5e7f0e9fc"
+  };
+  // Initialize Firebase
+const firebase = initializeApp(firebaseConfig);
+const database = getDatabase(firebase);
+
+export const useDbData = (path) => {
+    const [data, setData] = useState();
+    const [error, setError] = useState(null);
+  
+    useEffect(() => (
+      onValue(ref(database, path), (snapshot) => {
+       setData( snapshot.val() );
+      }, (error) => {
+        setError(error);
+      })
+    ), [ path ]);
+  
+    return [ data, error ];
+  };
+  
+  const makeResult = (error) => {
+    const timestamp = Date.now();
+    const message = error?.message || `Updated: ${new Date(timestamp).toLocaleString()}`;
+    return { timestamp, error, message };
+  };
+  
+  export const useDbUpdate = (path) => {
+    const [result, setResult] = useState();
+    const updateData = useCallback((value) => {
+      update(ref(database, path), value)
+      .then(() => setResult(makeResult()))
+      .catch((error) => setResult(makeResult(error)))
+    }, [database, path]);
+  
+    return [updateData, result];
+  };
